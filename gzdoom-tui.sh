2d11505fbe4e041c51716c2cf9763a6673e19684
TerseTears <tersetears@protonmail.com>
2022-02-10 16:50:49 +0330

    Add load and save menus

--- gzdoom-tui.sh
+++ gzdoom-tui.sh
@@ -1,12 +1,35 @@
 #!/usr/bin/env bash
 
+export NEWT_COLORS='
+border=yellow,red
+button=yellow,blue
+entry=white,blue
+checkbox=white,blue
+actcheckbox=brightred,yellow
+compactbutton=lightgray,blue
+listbox=white,blue
+actlistbox=black,yellow
+actsellistbox=white,yellow
+root=,gray
+roottext=brightcyan,green
+sellistbox=green,brightgreen
+textbox=lightgray,brightblue
+title=white,gray
+window=,blue
+'
+
+! [[ -e "modnames.csv" ]] && touch "modnames.csv"
+
+modspath=~/.config/gzdoom/Gameplay/
+levelspath=~/.config/gzdoom/Levels/
+
 return_indices() {
     local -n selection_array="$1"
     local -n indices="$2"
     local return_array=()
     for index in "${indices[@]}"
     do
-        return_array+=("${selection_array["$ind"]}")
+        return_array+=("${selection_array["$index"]}")
     done
     echo "${return_array[@]}"
 }
@@ -17,9 +40,9 @@ setup_list() {
     local _file_names=("${_file_names[@]%.*}")
 
     local _file_args=()
-    for ind in "${!_files[@]}"
+    for index in "${!_files[@]}"
     do
-        _file_args+=("$ind" "${_file_names["$ind"]}" OFF)
+        _file_args+=("$index" "${_file_names["$index"]}" OFF)
     done
     echo "${_file_args[@]}"
 }
@@ -46,14 +69,125 @@ folder_radview() {
         "Choose user's permissions" 20 78 20 \
         "${file_args[@]}" 3>&1 1>&2 2>&3))
 
-    echo "${files["$file_selection"]}"
+    ! [[ -z "$file_selection" ]] && echo "${files["$file_selection"]}"
     }
 
-#folder_checkview ~/.config/gzdoom/Gameplay/
+main_menu() {
+    local modcount=$(echo "Number of Loaded Mods:" "${#gameplay_mods[@]}")
+    echo $(whiptail --title "GZDoom TUI" --backtitle "$modcount"\
+    --menu "Choose option" 20 58 12 \
+        "gameplay" "Choose gameplay mods" \
+        "level" "Choose level mod" \
+        "save" "Save current selection"\
+        "load" "Load selection"\
+        "exit" "Exit and run" \
+         3>&1 1>&2 2>&3)
+}
 
-folder_radview ~/.config/gzdoom/Levels/
+save_menu() {
+    local inputbox=$(echo -e "Gameplay mods:\n\n""${gameplay_mods[@]##*/}\n\n"\
+    "Level:" "${level_mod##*/}")
+    echo $(whiptail --inputbox "$inputbox" \
+        28 39 "" --title "Save mods as name" 3>&1 1>&2 2>&3)
+}
 
-#gameplay_mods_selected=($(folder_view ~/.config/gzdoom/Gameplay/))
+load_menu() {
+    local modnames=($(awk -F, 'NR!=1 {print $1}' modnames.csv))
+    local levelnames=($(awk -F, 'NR!=1 {print $2}' modnames.csv))
+
+    local modlists
+
+    IFS=, read -a modlists <<< \
+        $(awk -F, 'BEGIN {ORS=","} NR!=1 {print $3}' modnames.csv)
+
+    local modsargs=()
+    for index in "${!modnames[@]}"
+    do
+        local modlist=()
+        IFS=" " read -a modlist <<< "${modlists["$index"]}"
+        local levelname="${levelnames["$index"]}"
+        levelname="${levelname%.*}"
+        local shortnames=("${levelname:0:8}")
+        for mod in "${modlist[@]}"
+        do
+            mod="${mod%.*}"
+            shortnames+=("${mod:0:8}")
+        done
+        modsargs+=("${modnames["$index"]}" "${shortnames[*]}" OFF)
+    done
+
+    local toload=($(whiptail --clear --title \
+        "Check list example" --radiolist \
+        "Choose user's permissions" 30 60 20 \
+        "${modsargs[@]}" \
+        3>&1 1>&2 2>&3))
+
+    echo "$toload"
+}
+
+save_mods() {
+    # inplace library is necessary since piping back to file
+    # only manages to add the last printed line
+    ./save_mods.awk -i inplace modnames.csv "$1" \
+        "${level_mod##*/}" "${gameplay_mods[*]##*/}"
+}
+load_level() {
+    local level_mod=$(awk -F, -v loadname="$1" \
+        '$1 == loadname {print $2}' modnames.csv)
+
+    local level_mod="${level_mod/#/"$levelspath"}"
+    echo "$level_mod"
+}
+
+load_mods() {
+    local modlist=($(awk -F, -v loadname="$1" \
+        '$1 == loadname {print $3}' modnames.csv))
+
+    local gameplay_mods=()
+    for mod in "${modlist[@]}"
+    do
+        gameplay_mods+=("${mod/#/"$modspath"}")
+    done
+
+    echo "${gameplay_mods[@]}"
+}
+
+menu="main"
+while [[ -n "$menu" ]]
+do
+    case "$menu" in
+        "main")
+            menu=$(main_menu);;
+        "gameplay")
+            gameplay_mods=($(folder_checkview "$modspath"))
+            menu="main";;
+        "level")
+            level_mod=$(folder_radview "$levelspath")
+            menu="main";;
+        "save")
+            savename=$(save_menu)
+            if [[ -n "$savename" ]]; then
+                save_mods "$savename"
+            fi
+            menu="main";;
+        "load")
+            loadname=$(load_menu)
+            if [[ -n "$loadname" ]]; then
+                level_name=$(load_level "$loadname")
+                gameplay_mods=($(load_mods "$loadname"))
+            fi
+            menu="main";;
+        "exit")
+            menu=""
+            gzdoom -file "$level_mod" "${gameplay_mods[@]}"
+            ;;
+
+    esac
+done
 
 
-#gzdoom -file "${gameplay_mods_selected[@]}"
+# gameplay_mods=($(folder_checkview ~/.config/gzdoom/Gameplay/))
+# 
+# level_mod=($(folder_radview ~/.config/gzdoom/Levels/))
+# 
+# gzdoom -file "${gameplay_mods[@]}" "$level_mod"
